ec5c660ca67fdddfdbebf3b9011096dbf7e193e0
vanessa.peng <vanessapeng93@gmail.com>
2018-11-13 16:35:20 +0800

    ready for heroku

--- Movie.js
+++ Movie.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = 'mongodb://paul:abc123@ds221242.mlab.com:21242/movies';
+const db = 'mongodb://vanessa:abc123@ds241133.mlab.com:41133/vanessamovie';
 
 mongoose
   .connect(
